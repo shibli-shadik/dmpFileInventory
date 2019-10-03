985fcb028620214a8f171ca5f767b7d66ad6ac8a
shibli <shibli.shadik@gmail.com>
2019-10-03 11:11:48 +0600

    Save records in file register table is added

--- src/dmpfileinventory/MonitorDirectory.java
+++ src/dmpfileinventory/MonitorDirectory.java
@@ -1,6 +1,8 @@
 package dmpfileinventory;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -10,22 +12,84 @@ import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MonitorDirectory
 {
-    private static String SRC_FOLDER = "F:\\projects\\dmp\\testing\\src";
+    private static String DB_CONN_URL = null;
+    private static String DB_USER = null;
+    private static String DB_PASS = null;
+    //private static String SRC_FOLDER = "F:\\projects\\dmp\\testing\\src";
+    private static String SRC_FOLDER = null;
     
     public static void main(String[] args)
     {
-        register();
+        readConfigFile();
+        readDirectory();
     }
     
-    public static void register()
+    private static void readConfigFile()
+    {
+        BufferedReader reader = null;
+        String[] splitLine;
+        
+        try 
+        {
+            String absPath = new File(MonitorDirectory.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            
+            String line = null;
+            //reader = new BufferedReader(new FileReader(absPath.substring(0, absPath.length() - "dmpFileInventory.jar".length())+ "/config.txt"));
+            reader = new BufferedReader(new FileReader("F:\\projects\\dmp\\config.txt"));
+                        
+            while((line = reader.readLine()) != null)
+            {
+                splitLine = line.split("#");
+                
+                if("SRC_FOLDER".equals(splitLine[0]))
+                {
+                    SRC_FOLDER = splitLine[1];
+                }
+                else if("DB_CONN_URL".equals(splitLine[0]))
+                {
+                    DB_CONN_URL = splitLine[1];
+                }
+                else if("DB_USER".equals(splitLine[0]))
+                {
+                    DB_USER = splitLine[1];
+                }
+                else if("DB_PASS".equals(splitLine[0]))
+                {
+                    DB_PASS = splitLine[1];
+                }
+            }
+            
+            reader.close();
+        } 
+        catch (Exception ex) 
+        {
+            Logger.getLogger(MonitorDirectory.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        finally 
+        {
+            try 
+            {
+                reader.close();
+            } 
+            catch (IOException ex) 
+            {
+                Logger.getLogger(MonitorDirectory.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    public static void readDirectory()
     {
         try 
         {
@@ -49,19 +113,22 @@ public class MonitorDirectory
                     if (StandardWatchEventKinds.ENTRY_CREATE.equals(event.kind())) 
                     {
                         fileName = event.context().toString();
-                        System.out.println("File Name:" + fileName);
+                        //System.out.println("File Name:" + fileName);
                         
                         file = new File(SRC_FOLDER + "\\" + fileName);
-                        System.out.println("File Size:" + file.length());
+                        //System.out.println("File Size:" + file.length());
                         
                         //attrs = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
                         //FileTime time = attrs.creationTime();
                         
+                        /*
                         String pattern = "yyyy-MM-dd HH:mm:ss";
                         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
                         String formatted = simpleDateFormat.format(new Date(file.lastModified()));
-
                         System.out.println( "The file creation date and time is: " + formatted );
+                        */
+                        
+                        saveRegister(fileName, new Timestamp(file.lastModified()), file.length());
                     }
                 }
                 
@@ -74,6 +141,38 @@ public class MonitorDirectory
         catch (IOException | InterruptedException ex) 
         {
             Logger.getLogger(MonitorDirectory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void saveRegister(String fileName, Timestamp lastModified, long size)
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Insert data
+            try (Connection conn = DriverManager.getConnection(DB_CONN_URL, DB_USER, DB_PASS))
+            {
+                String query = " insert into file_register (name, last_modified_at, size, total_line, status, created_at, updated_at)"
+                        + " values (?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                preparedStmt.setString(1, fileName);
+                preparedStmt.setTimestamp(2, lastModified);
+                preparedStmt.setLong(3, size);
+                preparedStmt.setInt(4, 0);
+                preparedStmt.setInt(5, FileStatus.NEW.ordinal());
+                
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                
+                preparedStmt.setTimestamp(6, timestamp);
+                preparedStmt.setTimestamp(7, timestamp);
+                
+                preparedStmt.execute();
+            }
+        }
+        catch (ClassNotFoundException | SQLException ex)
+        {
+            System.out.println(ex.toString());
         }
     }
 }
